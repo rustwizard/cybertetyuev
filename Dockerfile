4ead05d39a3bb4101867765f8c17db02335b6fe0
Mikhail Aksenov <thecoldwine@gmail.com>
2022-08-10 17:40:20 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:14-alpine
+
+RUN mkdir /app
+
+COPY index.js /app
+COPY package.json /app
+
+WORKDIR /app
+
+RUN npm install
+
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const requests = require('request-promise');
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/gaziki/, async (msg, _) => {
+    const chatId = msg.chat.id;
+
+    try {
+        const resp = await requests.get('https://www.powernext.com/data-feed/1467707/819/17', {
+            json: true
+        });
+
+        const history = resp.values;
+        const lastPrice = history[0].data[history[0].data.length - 1].y;
+        
+        bot.sendMessage(chatId, `Газики нынче по ${lastPrice} за ${resp.unit} согласно https://www.powernext.com/spot-market-data`);
+    } catch (error) {
+        console.log(error);
+        bot.sendMessage(chatId, 'Не могу узнать, что там с газиками');
+    }
+});
+
+const himarsikiOptions = [
+    'Это определенно HIMARS',
+    'На HIMARS указывает характерная маркировка и ромбовидные сегменты',
+    'Не, стопудово HIMARS',
+    'Скорее всего HIMARS',
+    'Не похоже на HIMARS, на HIMARS указывает характерная маркировка и ромбовидные сегменты, а тут...'
+];
+
+bot.onText(/\/himarsiki/, async (msg, _) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, himarsikiOptions[Math.floor(Math.random() * himarsikiOptions.length)]);
+});
+
+
+http.createServer(function (_, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Всем привет, это КиберТетюев"!');
+  res.end();
+}).listen(8080);
